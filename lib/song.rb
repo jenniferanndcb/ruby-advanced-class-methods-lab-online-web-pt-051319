4fba5627a74ce8f28cd3076630aa387851676532
Jen <jenniferanndcb@gmail.com>
2019-05-31 12:06:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,5 +34,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
+    if self.all.any? {|song| song.name == name}
+      self.find_by_name
+    else 
+      self.create_by_name
+    end
+  end
     
 end
